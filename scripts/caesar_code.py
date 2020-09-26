502a9ee7cec58a9fdd8236aea5b703c112bde4af
agniesia <s15038@pjwstk.edu.pl>
2020-09-26 10:42:40 +0200

    Main.py removed. Help shown instead plain text on run without parameters.

--- scripts/caesar_code.py
+++ scripts/caesar_code.py
@@ -1,30 +1,30 @@
+#!/usr/bin/env python3
+
 import argparse
-from pprint import pprint
 
 class CaesarCode:
 
-    __code_a = ord('a')
-    __code_capital_a = ord('A')
-    __difference = ord('z') - ord('a') + 1
-
     def __init__(self):
         self.parser = argparse.ArgumentParser()
+        self.__code_a = ord('a')
+        self.__code_capital_a = ord('A')
+        self.__difference = ord('z') - ord('a') + 1
 
     def decode(self, text, shift):
         return self.encode(text, -shift)
 
     def encode(self, text, shift):
-        result = ''
+        result = []
         for letter in text:
             if letter.islower():
-                result += self.inner_function(
-                    letter, self.__code_a, shift)
+                result.append(self.inner_function(
+                    letter, self.__code_a, shift))
             elif letter.isupper():
-                result += self.inner_function(
-                    letter, self.__code_capital_a, shift)
+                result.append(self.inner_function(
+                    letter, self.__code_capital_a, shift))
             else:
-                result += letter
-        return result
+                result.append(letter)
+        return ''.join(result)
 
     def inner_function(self, letter, code, shift):
         letter_numeric = (lambda x: ord(x) - code)(letter)
@@ -42,7 +42,8 @@ class CaesarCode:
             help=''' Shift by number''')
         self.parser.add_argument(
             '--text', type=str, required=True,
-            help='''Text to shift''' )
+            help='''Text to shift''')
+        self.parser.usage = self.parser.format_help()
 
     def run(self):
         self.cli()
@@ -52,4 +53,8 @@ class CaesarCode:
         elif args.option == 'decode':
             print(self.decode(args.text, args.offset))
         else:
-            pprint('Something went wrong!')
+            print(self.parser.print_usage())
+
+if __name__ == '__main__':
+    active_caesar_code = CaesarCode()
+    active_caesar_code.run()
